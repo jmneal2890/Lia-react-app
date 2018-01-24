213a338780f69f38c81414c1946c4a1f8f98d8ee
J Michael Neal <jmneal.audio@gmail.com>
2018-01-24 09:20:33 -0800

    rebase

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { link } from 'react-router-dom';
+import './Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+        <button className="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">Navbar</a>
+
+        <div className="navbar-collapse collapse" id="navbarsExampleDefault" aria-expanded="false" style="">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu" aria-labelledby="dropdown01">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+      </div>
+    </nav>
+    );
+  }
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar.jsx';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <h2>Welcome</h2>
+          <p>lkjsdf sdflkj asdf laksjdf asdflkjasdf adslfkj asdflksjdf  asdlfkj asdf laskdf asl;kj asdf asdlfkj asdfl asdfl kjsdf </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home
